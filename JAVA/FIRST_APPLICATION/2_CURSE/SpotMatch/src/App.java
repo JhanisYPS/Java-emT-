b62e0e842b28add7802091b64908a2453578512f
JhanisYPS <janainayasminps@outlook.com>
2024-03-06 15:52:08 -0300

    StickerMaker

--- JAVA/FIRST_APPLICATION/2_CURSE/SpotMatch/src/App.java
+++ JAVA/FIRST_APPLICATION/2_CURSE/SpotMatch/src/App.java
@@ -3,6 +3,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -27,12 +28,33 @@ public class App {
         List<Map<String, String>> filmsList = parser.parse(body);
         System.out.println(filmsList.size());
         System.out.println(filmsList.get(0));
-
+        
+        //Formatação de desafio
+        
 
         for (Map<String,String> film : filmsList) {
-            System.out.println(film.get("title"));
+            //Pagar Json Rating em formato string
+            String imDbRating = film.get("imDbRating");
+            //Transforma String em Double
+            double imDbStarRating = Double.parseDouble(imDbRating)/2;
+            //Arredonda o Double em valor inteiro
+            int imDbIntStarRating = (int)imDbStarRating;
+            //Em Python seria print("*"*imDbIntStartRating), mas em java:
+            // Declaração de uma variável do tipo char chamada 'a' e atribuição do valor '*' a ela.
+            char a = '*';
+            // Declaração de um array de caracteres chamado 'arrayDeAs' com tamanho igual à variável 'imDbIntStarRating'.
+            char[] arrayDeAs = new char[imDbIntStarRating];
+            // Preenche o array 'arrayDeAs' com o caractere '*' usando o método fill da classe Arrays.
+            Arrays.fill(arrayDeAs, a);
+            // Cria uma nova String chamada 'stringComOsAs' a partir do array 'arrayDeAs'.
+            String stringComOsAs = new String(arrayDeAs);
+            //\u001b[48;5;26m - Deixa o fundo do terminal azul
+            //\u001b[38;5;240m - Deixa a letra cinza
+            //\u001b[m - Indica final da formatação
+            System.out.println("\u001b[48;5;26m\u001b[38;5;240mtitle: \u001b[m"+"\u001b[48;5;26m"+film.get("title")+ " \u001b[m");
             System.out.println(film.get("image"));
-            System.out.println(film.get("imDbRating"));
+            System.out.println("\u001b[38;5;220m" + stringComOsAs + "\u001b[m");
+            
            
         }
     }
